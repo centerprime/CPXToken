2c1e5edbf9c0fbf018b19a12447671e7954f62c7
centerprime <support@centerprime.technology>
2020-12-24 16:38:14 +0900

    organize folders
    - orgonize folders

--- contracts/ERC677.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity 0.5.2; 
-import "./TokenERC20.sol";
-
-contract ERC677 is TokenERC20 {
-  function transferAndCall(address to, uint value, bytes memory data)  public returns (bool success);
-
-  event Transfer(address indexed from, address indexed to, uint value, bytes data);
-}
--- contracts/ERC677Receiver.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity 0.5.2; 
-
-contract ERC677Receiver {
-  function onTokenTransfer(address _sender, uint _value, bytes memory _data)  public;
-}
